e3a51ce338586e588bd333295f8a083c5b18748a
rbaryla <robert@em4u.pl>
2016-11-11 22:22:28 +0100

    Unicode

---
+++ src/unicode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rbaryla
+ * Date: 11.11.2016
+ * Time: 18:01
+ */
+
+echo "\u{20AC}", PHP_EOL;
+echo "\u{2706}", PHP_EOL;
+echo "\u{2620}", PHP_EOL;
+echo "\u{BD}", PHP_EOL;
